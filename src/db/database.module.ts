840ac25e115d05da6e04cb8d2b3c46b938c9757f
SyedMuhammadBilalGillani <syedmuhammadbilalgillani2003@gmail.com>
2025-03-19 22:46:53 +0500

    connect with env

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -1,11 +1,22 @@
 // src/db/database.module.ts
 import { Global, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 // import { RedisModule } from './redis.module';
 
 @Global()
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pos_super_admin_db')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   exports: [MongooseModule],
 })
 export class DatabaseModule {}
